cc31aab7f6b9b000a6272882678095369f6cd35c
TripNRaVeR <noerialexeew@live.nl>
2013-07-03 20:28:45 +0200

    fs: improve generic fsync
    Conflicts:
    
    	fs/libfs.c

--- fs/libfs.c
+++ fs/libfs.c
@@ -912,12 +912,8 @@ int generic_file_fsync(struct file *file, int datasync)
 	int ret;
 
 	ret = sync_mapping_buffers(inode->i_mapping);
-	if (!(inode->i_state & I_DIRTY))
-		return ret;
-	if (datasync && !(inode->i_state & I_DIRTY_DATASYNC))
-		return ret;
-
 	err = sync_inode_metadata(inode, 1);
+
 	if (ret == 0)
 		ret = err;
 	return ret;
